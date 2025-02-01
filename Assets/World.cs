45cbf814576ab4ae93c18bf70afdc5b940bf4d21
pabloav57 <pabloav57@gmail.com>
2025-02-01 22:00:50 +0100

    Complex Behaviours and Behaviour Challenge

--- Assets/World.cs
+++ Assets/World.cs
@@ -1,12 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
+//A singleton to hold an array of all the hiding spots in the game environment
 public sealed class World
 {
     private static readonly World instance = new World();
     private static GameObject[] hidingSpots;
 
+    //construct the singleton
     static World()
     {
+        //populate hiding spot array with objects in the environment
+        //that match the tag
         hidingSpots = GameObject.FindGameObjectsWithTag("hide");
     }
 
@@ -14,9 +20,9 @@ public sealed class World
 
     public static World Instance
     {
-        get { return instance;}
+        get { return instance; }
     }
-    
+
     public GameObject[] GetHidingSpots()
     {
         return hidingSpots;
